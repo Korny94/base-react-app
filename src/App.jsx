7214cd092d1a0b7c7a1d25ff7dbb19aa33ade6c7
Korny94 <karlmagnusnokling@hotmail.no>
2024-02-22 23:37:27 +0100

    wef

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,5 @@
 import "./styles.css";
 
-function App() {
-  return (
-    <>
-      <form className="new-item-form">
-        <div className="form-row">
-          <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
-        </div>
-        <button className="btn">Add</button>
-      </form>
-      <h1 className="header">Todo List</h1>
-      <ul className="list">
-        <li>
-          <label>
-            <input type="checkbox" />
-            <span>Item 1</span>
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox" />
-            <span>Item 2</span>
-          </label>
-          <button className="btn btn-danger">Delete</button>
-        </li>
-      </ul>
-    </>
-  );
-}
+function App() {}
 
 export default App;
